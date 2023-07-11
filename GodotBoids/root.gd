1e70f4c00a329367582d498900dfaca5b5a2349b
Bryan Duggan <skooter500@gmail.com>
2023-07-11 22:45:25 +0100

    materials

--- GodotBoids/root.gd
+++ GodotBoids/root.gd
@@ -1,28 +1,65 @@
 extends Node3D
 
 @export var custom_font : Font
+@export var zylann_example := false
+@export var test_text := true
+@export var test_graphs := false
+@export var more_test_cases := true
+@export var draw_array_of_boxes := false
+@export_range(0, 1024) var start_culling_distance := 0.0
 
+@export_group("Text groups", "text_groups")
+@export var text_groups_show_hints := true
+@export var text_groups_show_stats := true
+@export var text_groups_position := DebugDrawConfig2D.POSITION_LEFT_TOP
+@export var text_groups_offset := Vector2i(8, 8)
+@export var text_groups_padding := Vector2i(3, 1)
+@export_range(1, 100) var text_groups_default_font_size := 12
+@export_range(1, 100) var text_groups_title_font_size := 14
+@export_range(1, 100) var text_groups_text_font_size := 12
+
+@export_category("Graphs")
+@export var graph_offset := Vector2i(8, 8)
+@export var graph_size := Vector2i(200, 80)
+@export_range(1, 100) var graph_title_font_size := 14
+@export_range(1, 100) var graph_text_font_size := 12
+@export_range(0, 64) var graph_text_precision := 2
+@export_range(1, 32) var graph_line_width := 1.0
+@export_range(1, 512) var graph_buffer_size := 128
+@export var graph_frame_time_mode := true
+@export var graph_is_enabled := true
 
 func draw_gizmos():
-	var size = 100
-	var sub_divisions = 10
+	var size = 5000
+	var sub_divisions = size / 100
 	DebugDraw.draw_grid(Vector3.ZERO, Vector3.RIGHT* size, Vector3.BACK * size, Vector2(sub_divisions, sub_divisions), Color.AQUAMARINE)
+	
 	# DebugDraw.draw_grid(Vector3.ZERO, Vector3.UP * size, Vector3.BACK * size, Vector2(sub_divisions, sub_divisions), Color.aquamarine)
 	# DebugDraw.draw_grid(Vector3.ZERO, Vector3.RIGHT* size, Vector3.BACK * size, Vector2(sub_divisions, sub_divisions), Color.AQUAMARINE)
 
+func _upd_graph_params():
+	DebugDraw.config_2d.graphs_base_offset = graph_offset
+	for g in [&"FPS", &"fps5", &"fps8"]:
+		var graph := DebugDraw.get_graph(g) as DebugDrawFPSGraph
+		if graph:
+			graph.size = graph_size
+			graph.title_size = graph_title_font_size
+			graph.text_size = graph_text_font_size
+			graph.line_width = graph_line_width
+			graph.text_precision = graph_text_precision
+			graph.buffer_size = graph_buffer_size
+			if Engine.is_editor_hint() or g != &"FPS":
+				graph.frame_time_mode = graph_frame_time_mode
+
 
 func _ready():
-	get_window().set_current_screen(0)
-	var screen_size = DisplayServer.screen_get_size()
-	var window_size = get_window().get_size()
-	
-	get_window().set_position(screen_size*0.5 - window_size*0.5) 	
-	# DebugDraw.tex = custom_font
+	get_window().set_current_screen(1)
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	draw_gizmos()
 	var g = _create_graph(&"FPS", true, false, DebugDrawGraph.TEXT_CURRENT | DebugDrawGraph.TEXT_AVG | DebugDrawGraph.TEXT_MAX | DebugDrawGraph.TEXT_MIN, &"", DebugDrawGraph.SIDE_BOTTOM, DebugDrawGraph.POSITION_LEFT_TOP if Engine.is_editor_hint() else DebugDrawGraph.POSITION_RIGHT_TOP, Vector2i(200, 80), custom_font)
-	g.buffer_size = 300
+	g.frame_time_mode = false
 
 func _create_graph(title, is_fps, show_title, flags, parent := &"", parent_side := DebugDrawGraph.SIDE_BOTTOM, pos = DebugDrawGraph.POSITION_LEFT_BOTTOM, size := Vector2i(256, 60), font = null) -> DebugDrawGraph:
 	var graph := DebugDraw.get_graph(title)
