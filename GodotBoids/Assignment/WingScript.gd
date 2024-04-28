156424eb504c1bc990dccf63e7fcfbd4ea5f0f05
Daniel Kondabarov <drk3nlzux@relay.firefox.com>
2024-04-28 19:29:26 +0100

    Draw the side faces

--- GodotBoids/Assignment/WingScript.gd
+++ GodotBoids/Assignment/WingScript.gd
@@ -21,6 +21,7 @@ var invert: bool
 
 var amplitude: float = 1.0
 var frequency: float = 16.0
+# Broken?
 var phase_shift: float = 2.0
 var vertical_shift: float = 1.0
 
@@ -103,45 +104,81 @@ func _process(delta: float) -> void:
 	mesh.clear_surfaces()
 	mesh.surface_begin(Mesh.PRIMITIVE_TRIANGLES)
 
-	draw_new_face(top_vertices)
-	draw_new_face(bottom_vertices)
-	draw_new_face(front_vertices)
-	draw_new_face(back_vertices)
-	draw_new_face(left_vertices)
-	draw_new_face(right_vertices)
+	draw_faces(top_vertices)
+	create_wing()
+	#draw_new_face(bottom_vertices)
+	#draw_new_face(front_vertices)
+	#draw_new_face(back_vertices)
+	#draw_new_face(left_vertices)
+	#draw_new_face(right_vertices)
 
 	# End drawing.
 	mesh.surface_end()
 
-func draw_new_face(vertices: Array):
+func draw_faces(vertices: Array):
 	for i in range(len(vertices)):
 		mesh.surface_set_normal(Vector3(0, 1, 0))
 		mesh.surface_set_uv(uvs[i%3])
 		mesh.surface_add_vertex(vertices[i])
 
-func create_sin_points()-> void:
-	sin_points.clear()
+func create_sin_points()-> Array:
+	var points = []
 	# Make sure the wings stay in place
 	amplitude = pos
 	vertical_shift = pos
 	for x in range(num_of_points):
 		var y = amplitude * sin((x-frequency)/phase_shift)+vertical_shift
+		#var y
+		#if x == 0:
+			#y = amplitude * sin((x-frequency))+vertical_shift
+		#else:
+			#y = amplitude * sin((x-frequency)/phase_shift)+vertical_shift
 		if invert:
-			sin_points.append(Vector3(-(x*dist_between_points),y,0))
+			points.append(Vector3(-(x*dist_between_points),y,0))
 		else:
-			sin_points.append(Vector3(x*dist_between_points,y,0))
-	
-func create_fronts(next_point: Vector2, prev_point: Vector3):
+			points.append(Vector3(x*dist_between_points,y,0))
+	return points
+
+func create_wing():
+	var points = create_sin_points()
+	for i in range(num_of_points):
+		if i == 0:
+			continue
+		var next_point = points[i]
+		var prev_point = points[i-1]
+		var vertices = create_fronts(next_point, prev_point)
+		draw_faces(vertices)
+
+func create_fronts(next_point: Vector3, prev_point: Vector3):
 	var offset = tickness/2
-	var witdh_offset = witdh/2
+	var width_offset = witdh/2
 	# a,b,c,d are the four corners in the rectangle. Each point in the sin point
 	# represent the midpoint between the corners. a and b are top corners while
 	# c and d are bottom corners
-	var a = next_point+offset
-	var b = prev_point+offset
-	var c = next_point-offset
-	var d = prev_point-offset
+	var a = next_point.y+offset
+	var b = prev_point.y+offset
+	var c = next_point.y-offset
+	var d = prev_point.y-offset
 
 	var vertices = []
 	# Construct the triangles
-	vertices.append(Vector3())	
+	# Front face
+	# First triangle
+	vertices.append(Vector3(next_point.x,a,width_offset))
+	vertices.append(Vector3(prev_point.x,b,width_offset))
+	vertices.append(Vector3(next_point.x,c,width_offset))
+	# Second triangle
+	vertices.append(Vector3(prev_point.x,b,width_offset))
+	vertices.append(Vector3(prev_point.x,d,width_offset))
+	vertices.append(Vector3(next_point.x,c,width_offset))
+	# Back face
+	# First triangle	
+	vertices.append(Vector3(next_point.x,a,-width_offset))
+	vertices.append(Vector3(next_point.x,c,-width_offset))
+	vertices.append(Vector3(prev_point.x,b,-width_offset))
+	# Second triangle
+	vertices.append(Vector3(prev_point.x,b,-width_offset))
+	vertices.append(Vector3(next_point.x,c,-width_offset))
+	vertices.append(Vector3(prev_point.x,d,-width_offset))
+	
+	return vertices
