d7bb2513de3d9f68c919c666e92c39da3157ffbf
Daniel Kondabarov <drk3nlzux@relay.firefox.com>
2024-04-29 15:26:23 +0100

    Create walking and flying states

--- GodotBoids/Assignment/BodyAnimator.gd
+++ GodotBoids/Assignment/BodyAnimator.gd
@@ -10,6 +10,10 @@ var wing1: Wing
 var wing2: Wing
 @export
 var body: Node3D
+@export
+var druation: float = 1
+@export
+var wing_span: int = 5
 
 var tween: Tween 
 var current_state: String = "Walking"
@@ -26,14 +30,40 @@ func _process(delta: float) -> void:
 		return
 	match state:
 		"Flying":
+			current_state = "Flying"
 			transition_to_flying()
 		"Walking":
+			current_state = "Walking"
 			transition_to_walking()
 		_:
 			print("Unsupported body state. Please make sure that ",state," is a supported state") 
 	
 func transition_to_walking():
-	pass
+	if (tween == null or not tween.is_running()): 
+		tween = get_tree().create_tween()
+		tween.set_ease(Tween.EASE_IN)
+		tween.set_trans(Tween.TRANS_LINEAR)
+		tween.parallel().tween_property(wing1, "position", Vector3(0,0.071,-0.354), druation)
+		tween.parallel().tween_property(wing2, "position", Vector3(0,0.071,-0.354), druation)
+		tween.parallel().tween_property(body, "position", Vector3(0,0,0), druation)
+		tween.parallel().tween_property(wing1, "rotation_degrees", Vector3(-45,0,0), druation)
+		tween.parallel().tween_property(wing2, "rotation_degrees", Vector3(45,180,0), druation)
+		tween.parallel().tween_property(body, "rotation_degrees", Vector3(0,0,0), druation)
+		tween.parallel().tween_property(wing1, "num_of_points", 1, druation)
+		tween.parallel().tween_property(wing2, "num_of_points", 1, druation)
+		tween.play()
 	
 func transition_to_flying():
-	pass
+	if (tween == null or not tween.is_running()): 
+		tween = get_tree().create_tween()
+		tween.set_ease(Tween.EASE_IN)
+		tween.set_trans(Tween.TRANS_LINEAR)
+		tween.parallel().tween_property(wing1, "position", Vector3(0,1.621,-0.354), druation)
+		tween.parallel().tween_property(wing2, "position", Vector3(0,1.621,-0.354), druation)
+		tween.parallel().tween_property(body, "position", Vector3(0,1.29,-0.336), druation)
+		tween.parallel().tween_property(wing1, "rotation_degrees", Vector3(0,0,0), druation)
+		tween.parallel().tween_property(wing2, "rotation_degrees", Vector3(0,180,0), druation)
+		tween.parallel().tween_property(body, "rotation_degrees", Vector3(45,0,0), druation)
+		tween.parallel().tween_property(wing1, "num_of_points", wing_span, druation)
+		tween.parallel().tween_property(wing2, "num_of_points", wing_span, druation)
+		tween.play()
