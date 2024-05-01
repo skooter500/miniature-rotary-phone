7b7d0eec1b0d33492174fd1411965d8c7252d831
Olabode Balinga <98917947+Rezix15@users.noreply.github.com>
2024-05-01 16:14:17 +0100

    Added in robot and other features.

--- GodotBoids/robot/Robot.gd
+++ GodotBoids/robot/Robot.gd
@@ -1,19 +1,23 @@
 extends Node3D
 
-@export var isTrigger = false
+
+var isTrigger = false
 
 var currentState: int
 var triggered = true
 
-var closest_bird : Node3D
-var closest_distance : float
-
+var canShoot = false
 
 @export var pursueColor : Color
 @export var wanderColor: Color
 
+@export var murderNode : Node3D
+
 var robotMesh : MeshInstance3D
 var eyeMat : StandardMaterial3D
+var pursue 
+var constrain
+var birds 
 
 @export var laserbeamL : Node3D
 @export var laserbeamR : Node3D
@@ -23,17 +27,23 @@ func _ready():
 	laserbeamR.visible = false
 	currentState = 0
 	robotMesh = get_node("Robot/EnemyVer2")
+	pursue = find_child("Pursue")
+	# constrain = find_child("Constrain")
+	
 	
 	eyeMat = robotMesh.get_surface_override_material(1)
+	
+	_findBirds()
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+	_checkDistance()
 	if isTrigger:
 		eyeMat.emission = pursueColor
 		
-		if triggered:
+		if triggered and canShoot:
 			laserbeamL.visible = true
 			laserbeamR.visible = true
 			triggered = false
@@ -42,10 +52,27 @@ func _process(delta):
 		laserbeamL.visible = false
 		laserbeamR.visible = false
 		triggered = false
+		canShoot = false
 	
 
 func _findBirds():
-	var root = get_node(".").get_children()
-	var birds = root
 	
-	
+	var murder = get_node("../Murder")
+	#
+	if murder.get_child_count() > 0:
+		birds = murder.get_child(0)
+		pursue.enemy_boid = birds
+		#pursue.enemyNodePath = 
+		isTrigger = true
+		print("Birds found")
+	else:
+		isTrigger = false
+		print("No birds found")
+		
+
+# Funciton to check the nearest distance to the bird and shoot
+func _checkDistance():
+	if global_transform.origin.distance_to(birds.global_transform.origin) < 30:
+		canShoot = true
+	else:
+		canShoot = false
